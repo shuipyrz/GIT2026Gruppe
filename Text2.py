9b5252603c16856dc862969d96e6da697487a1ba
shuipyrz <runziyan5@gmail.com>
2026-04-27 14:54:56 +0800

    it is second test

---
+++ Text2.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.write("Hello Streamlit" )
+st.write(123123)
+st.write({"a":1, "b":2})
+st.write({1, 2, "apple", "banana"})
+
+# just put results here
+{"a":1, "b":2}
+[1, 2, "apple", "banana"]
