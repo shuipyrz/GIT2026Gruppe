b111cc210329021d8397b1a9d1531ef0a878fce1
Chuanbin Zheng <bin869269@gmail.com>
2026-05-02 22:46:16 +0200

    260502

--- ChuanbinZheng/test0.py
+++ ChuanbinZheng/test0.py
@@ -41,7 +41,7 @@ llm = ChatOpenAI(
 
 def parse_request(text: str):
     """
-    Wandelt Nutzertext in GIS-JSON um.
+    Analysieren die Anforderungen der Nutzer:innen und bestimmen, welche Art von Karten Nutzer:innen erstellen möchten
     """
 
     system_prompt = """
@@ -62,26 +62,33 @@ def parse_request(text: str):
     2. ROUTE (Linien):
     {
     "type": "route",
-    "data": {
+    "data": [
+    {
+        "name": "string",
         "points": [
         {"lat": float, "lon": float}
         ]
     }
+    ]
     }
 
     3. AREA (Flächen):
     {
     "type": "area",
-    "data": {
-        "polygon": [
-        [lat, lon]
+    "data": [
+    {
+        "name": "string",
+        "points": [
+        {"lat": float, "lon": float}
         ]
     }
+    ]
     }
 
     Regeln:
     - KEINE anderen Felder erlauben
     - KEIN Text außerhalb JSON
+    - Die Ausgabe muss ein einzelnes JSON-Objekt sein (beginnend mit { und endend mit }).
     """
 
     response = llm.invoke([
@@ -95,6 +102,68 @@ def parse_request(text: str):
         return json.loads(content)
     except json.JSONDecodeError:
         return {"error": "invalid JSON", "raw": content}
+
+
+# erstellen Punktekarte
+def erstellen_karten_Punkte(Daten:dict, m:folium.Map) -> None:
+    for point in Daten["data"]:
+            folium.CircleMarker(
+                location=[point["lat"], point["lon"]],
+                radius=12,
+                color="red",
+                fill=True,
+                fill_color="orange",
+                fill_opacity=0.7,
+                tooltip=folium.Tooltip(point["name"], permanent=False),
+                popup=point["name"]
+            ).add_to(m)
+    # zoom   
+    points=[[point["lat"],point["lon"]] for point in Daten["data"]] 
+    m.fit_bounds(points) 
+
+# erstellen Routenkarte
+def erstellen_karten_Routen(Daten:dict, m:folium.Map) -> None:
+    Routen=Daten["data"]
+    alle_Punkte=[]
+    for Route in Routen:
+        Points_der_Route=[[p["lat"], p["lon"]] for p in Route["points"]]
+        # Points_der_Route=[Route["points"]["lat"],Route["points"]["lon"]]
+        folium.PolyLine(
+            Points_der_Route,
+            color="blue", 
+            weight=5, 
+            opacity=0.8,
+            tooltip=folium.Tooltip(Route["name"], permanent=False),
+            popup=Route["name"]
+        ).add_to(m)
+        alle_Punkte.extend(Points_der_Route)
+    # zoom    
+    if alle_Punkte:
+        m.fit_bounds(alle_Punkte) 
+
+
+
+# erstellen Regionskarte
+def erstellen_karten_Region(Daten:dict, m:folium.Map) -> None:
+    Regionen=Daten["data"]
+    alle_Punkte=[]
+    for Region in Regionen:
+        Points_des_Regions=[[p["lat"], p["lon"]] for p in Region["points"]]
+        # Points_der_Route=[Route["points"]["lat"],Route["points"]["lon"]]
+        folium.Polygon(
+            Points_des_Regions,
+            color="green", 
+            fill=True,
+            fill_color="green",
+            fill_opacity=0.4,
+            tooltip=folium.Tooltip(Region["name"], permanent=False),
+            popup=Region["name"]
+        ).add_to(m)
+        alle_Punkte.extend(Points_des_Regions)
+    # zoom    
+    if alle_Punkte:
+        m.fit_bounds(alle_Punkte)
+    
 
 # mit der erstellung anfangen
 st.title("Karten erstellen")
@@ -121,20 +190,20 @@ if st.button("Karten erstellen"):
     with st.spinner("In Bearbeitung..."):    
         st.session_state.data = parse_request(user_input)
         Daten=st.session_state.data
-        points=[[point["lat"],point["lon"]] for point in Daten["data"]] 
+
         m = folium.Map()
-        for point in Daten["data"]:
-            folium.CircleMarker(
-                location=[point["lat"], point["lon"]],
-                radius=12,
-                color="red",
-                fill=True,
-                fill_color="orange",
-                fill_opacity=0.7,
-                tooltip=folium.Tooltip(point["name"], permanent=False),
-                popup=point["name"]
-            ).add_to(m)
-        m.fit_bounds(points) 
+        if Daten["type"]=="poi":
+            erstellen_karten_Punkte(Daten,m)
+        elif Daten["type"]=="route":
+            erstellen_karten_Routen(Daten,m)
+        elif Daten["type"]=="area":
+            erstellen_karten_Region(Daten,m)
+        else:
+            placeholder3 = st.empty()
+            placeholder3.write("Entschudigung, ich kann jetzt diese Art von Karten leider nicht erstellen!")
+            time.sleep(5)
+            placeholder3.empty()
+            st.stop()
 
         st.session_state.map_created=True
         st.session_state.map=m
